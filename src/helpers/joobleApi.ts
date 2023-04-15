32ca272138dfc2935cabd7019ab8cacc6a88b72b
Grzegorz Wozniak <grubysweter@gmail.com>
2023-04-15 10:04:12 +0200

    update

--- src/helpers/joobleApi.ts
+++
@@ -1,8 +0,0 @@
-// export const JOOBLE_API_US = "a63e938c-c8d2-40c4-84ba-a94129d2ad24";
-// export const JOOBLE_API = "382e433b-a80d-4f87-82c0-628dbd9ddc88";
-
-// Twój unikalny klucz API:
-//  "382e433b-a80d-4f87-82c0-628dbd9ddc88"
-// Pamiętaj, że API ma domyślny limit 500 wyszukiwań. Jeśli chcesz
-// zwiększyć liczbę wyszukiwań lub masz inne pytania, napisz do nas
-// w odpowiedzi na ten list. Z wyrazami szacunku, Zespół Jooble
