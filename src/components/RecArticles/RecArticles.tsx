f928112962b2a4d0f5993a2d976234d5f914a3b0
Grzegorz Wozniak <grubysweter@gmail.com>
2023-04-17 13:42:13 +0200

    <RECENT HISTORY> name updated

--- src/components/RecArticles/RecArticles.tsx
+++ src/components/RecArticles/RecArticles.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, List, ListItem } from "@mui/material";
+import { Button, Divider, List, ListItem, Typography } from "@mui/material";
 import { doc, deleteDoc } from "firebase/firestore";
 import React from "react";
 import { db } from "../../helpers/firebaseConfig";
@@ -27,67 +27,78 @@ export const RecArticles = ({ rec }: RecProps) => {
     console.log("Deleted");
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignContent: "center",
-        justifyContent: "center",
-        fontFamily: "monospace",
-      }}
-    >
-      <a
-        key={rec.id}
-        href={rec.link}
-        target="_blank"
+    <>
+      <Typography
+        variant="h2"
+        align="center"
+        sx={{ fontFamily: "monospace", fontSize: "2rem", mt: ".8rem" }}
+      >
+        Recent History
+      </Typography>
+      <div
         style={{
-          textAlign: "center",
-          textDecoration: "none",
           display: "flex",
           flexDirection: "column",
-          margin: "10px 25px 5px 25px",
+          alignContent: "center",
           justifyContent: "center",
+          fontFamily: "monospace",
         }}
       >
-        <span
+        <a
+          key={rec.id}
+          href={rec.link}
+          target="_blank"
           style={{
-            fontWeight: "bold",
-            fontSize: "15px",
-            color: "#1976d2",
+            textAlign: "center",
+            textDecoration: "none",
+            display: "flex",
+            flexDirection: "column",
+            margin: "10px 25px 5px 25px",
+            justifyContent: "center",
           }}
         >
-          {rec.company}
-        </span>
-        <span style={{ fontWeight: "bold", fontSize: "17px", color: "black" }}>
-          {rec.title}
-        </span>
-        <span
+          <span
+            style={{
+              fontWeight: "bold",
+              fontSize: "15px",
+              color: "#1976d2",
+            }}
+          >
+            {rec.company}
+          </span>
+          <span
+            style={{ fontWeight: "bold", fontSize: "17px", color: "black" }}
+          >
+            {rec.title}
+          </span>
+          <span
+            style={{
+              fontSize: "12px",
+              color: "#1976d2",
+            }}
+          >
+            {rec.location}
+          </span>
+          <span
+            style={{
+              fontSize: "10px",
+              color: "grey",
+              fontStyle: "italic",
+            }}
+          >
+            {rec.source}
+          </span>
+        </a>
+        <Button onClick={deleteData}>delete</Button>
+        <Divider
           style={{
-            fontSize: "12px",
-            color: "#1976d2",
+            backgroundColor: "#1976d2",
+            width: "100%",
+            opacity: "25%",
           }}
-        >
-          {rec.location}
-        </span>
-        <span
-          style={{
-            fontSize: "10px",
-            color: "grey",
-            fontStyle: "italic",
-          }}
-        >
-          {rec.source}
-        </span>
-      </a>
-      <Button onClick={deleteData}>delete</Button>
-      <Divider
-        style={{
-          backgroundColor: "#1976d2",
-          width: "100%",
-          opacity: "25%",
-        }}
-      />
-    </div>
+        />
+      </div>
+    </>
   );
 };
 
